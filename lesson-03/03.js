15d808cffa7ac27d78862265fa2c434b07a35edb
AnastasiaKolesnikowa <an_kolesnikowa@mail.ru>
2024-09-08 17:20:57 +0300

    js_4

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -3,8 +3,8 @@
 function findLargest(a, b, c) {
     return Math.max(a, b, c);
 }
-const nam1 = 56;
-const nam2 = 52;
-const nam3 = 32;
+const nam1 = +prompt("First number");
+const nam2 = +prompt("Second number");
+const nam3 = +prompt("Third number");
 const max = findLargest(nam1, nam2, nam3);
 console.log(max);
