be93d194bacccaf951a7978b4f50be6ce7654ae6
AnastasiaKolesnikowa <an_kolesnikowa@mail.ru>
2024-09-11 00:24:36 +0300

    task

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -17,32 +17,42 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-const num1 = [1, 2, 3];
-const num2 = [2, 3, 4];
+const arr1 = ["apple", "orange"];
+const arr2 = ["banana", "apple"];
 
-function findCommonElements(array1, array2) {
-    const array3 = array1.concat(array2);
-    return array3;
+function includesElement(array, element) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === element) {
+            return true;
+        }
+    }
+    return false;
 }
 
-const array3 = findCommonElements(num1, num2);
-console.log(array3);
+function findCommonElements(array1, array2) {
+    const newArr = [];
+    for (let i = 0; i < array1.length; i++) {
+        if (includesElement(array2, array1[i])) {
+            newArr.push(array1[i]);
+        }
+    }
+    return newArr;
+}
 
-const array4 = array3.filter((item, index) => {
-    return array3.indexOf(item) !== index;
-});
-console.log(array4);
+console.log(findCommonElements(arr1, arr2));
 
-// function newArray(array) {
-//     const array = array3.filter((item, index) => {
-//         return array.indexOf(item) !== index;
-//     });
+// const num1 = [1, 2, 3];
+// const num2 = [2, 3, 4];
+
+// function findCommonElements(array1, array2) {
+//     const array3 = array1.concat(array2);
+//     return array3;
 // }
-// const array4 = newArray(array3);
 
-// console.log(array4);
+// const array3 = findCommonElements(num1, num2);
+// console.log(array3);
 
-// array3 = array3.filter((item, index) => {
+// const array4 = array3.filter((item, index) => {
 //     return array3.indexOf(item) !== index;
 // });
-// console.log(array3);
+// console.log(array4);
