e9746bbdfa00a40b241f23ec74c0521119259b68
AnastasiaKolesnikowa <an_kolesnikowa@mail.ru>
2024-10-12 20:00:19 +0300

    task_9

--- lesson-09/02.js
+++ lesson-09/02.js
@@ -12,16 +12,22 @@
 Ваша функция `map` должна возвращать новый массив, где каждый элемент является результатом вызова функции `callback` на каждом элементе исходного массива.
 */
 
-/*
-Пример использования вашей функции:
+// Пример использования вашей функции:
 
-const numbers = [1, 2, 3, 4, 5]
+// Должен вывести: [2, 4, 6, 8, 10]
+
+const numbers = [1, 2, 3, 4, 5];
+
+const map = (array, callback) => {
+    const newArray = [];
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        newArray.push(callback(element, index));
+    }
+    return newArray;
+};
 
 const doubledNumbers = map(numbers, (element, index) => {
-  return element * 2
-})
-
-console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
-*/
-
-const map = () => {}
+    return element * 2;
+});
+console.log(doubledNumbers);
